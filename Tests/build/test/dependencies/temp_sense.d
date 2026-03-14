1d62c5f3f53a815dca8cffb9c40e437324e4f3b1
Kamil Jarosz <kjarosz19946@gmail.com>
2026-03-13 20:45:32 -0400

    Remove build folder from tracking

--- Tests/build/test/dependencies/temp_sense.d
+++
@@ -1,2 +0,0 @@
-build/test/out/test_temp_sense/temp_sense.o: ../Core/Src/temp_sense.c \
- ../Core/Inc/temp_sense.h
--- Tests/build/test/dependencies/test_temp_sense.d
+++
@@ -1,3 +0,0 @@
-build/test/out/test_temp_sense/test_temp_sense.o: test/test_temp_sense.c \
- build/vendor/unity/src/unity.h build/vendor/unity/src/unity_internals.h \
- ../Core/Inc/temp_sense.h
